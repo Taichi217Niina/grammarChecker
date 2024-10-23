8102abfc7ee0994b79d2569ae7bfedf83ee61d6b
tayboy217 <taichi217niina@gmail.com>
2024-10-23 20:23:49 +0900

    first commit

---
+++ src/main/java/com/grammar/checker/GrammarCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.grammar.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammarCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrammarCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grammar/checker/GrammarCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grammar.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrammarCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
